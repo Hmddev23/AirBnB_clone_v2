259dc9a2d45944dc2216f734d47f5d0c37c071a5
Hmddev23 <hamid.maira709@gmail.com>
2024-02-18 16:56:30 -0800

    last task

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+"""
+11. HBNB filters
+start a Flask web application.
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    display “Hello HBNB!”
+    """
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    return HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    replace any underscores in <text> with slashes.
+    """
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    replace any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    displays 'n is a number' only if n is an integer.
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ def doc """
+    if n % 2 == 0:
+        p = 'even'
+    else:
+        p = 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=p)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    display an HTML page with a list of all State objects in DBStorage.
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close(error):
+    """
+    remove the current SQLAlchemy session.
+    """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    display an HTML page with a list of all states and related cities.
+    """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    """
+    display an HTML page with info about <id>, if it exists.
+    """
+    not_found = False
+    if id is not None:
+        states = storage.all(State, id)
+        with_id = True
+        if len(states) == 0:
+            not_found = True
+    else:
+        states = storage.all(State)
+        with_id = False
+    return render_template('9-states.html', states=states,
+                           with_id=with_id, not_found=not_found)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb2():
+    """
+    display the main HBnB HTML page.
+    """
+    amenities = storage.all(Amenity)
+    states = storage.all(State)
+    return render_template('100-hbnb.html', states=states, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
